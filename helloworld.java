da7156db2fde28658bf7ceceb8bebae412296a3c
Anburaj2202 <36660890+Anburaj2202@users.noreply.github.com>
2018-02-20 19:33:31 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Hello Anbu Powershell");
+        System.out.println("Hello Anbu name Powershell");
  //creating object
   }
 }
