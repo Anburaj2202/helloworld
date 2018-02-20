04cee0730f43b8ecc93e41888e15917e6cd33cca
Anburaj2202 <36660890+Anburaj2202@users.noreply.github.com>
2018-02-20 19:06:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Anbu Jenkins");
  //creating object
   }
 }
